8acd011d33e359022634a16731817a7f8aee2e82
cielo-illust <the_illustrations_of_cielo@hotmail.co.jp>
2016-01-08 23:19:49 +0900

    init

---
+++ Assets/Scripts/CandyDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandyDestroyer : MonoBehaviour {
+	public CandyHolder candyHolder;
+	public int reward;
+	public GameObject effectPrefab;
+	public Vector3 effectRotation;
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Candy") {
+			candyHolder.AddCandy (reward);
+			Destroy (other.gameObject);
+			if (effectPrefab != null) {
+				Instantiate (
+					effectPrefab,
+					other.transform.position,
+					Quaternion.Euler(effectRotation)
+				);
+			}
+		}
+	}
+}
